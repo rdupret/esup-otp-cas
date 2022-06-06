1847e8dcfdad715929a88b4b92d53049b19272db
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2022-06-06 22:17:03 +0200

    fix running EsupOtpBypassProvider if TGT exists

--- src/main/java/org/esupportail/cas/config/support/authentication/EsupOtpBypassProvider.java
+++ src/main/java/org/esupportail/cas/config/support/authentication/EsupOtpBypassProvider.java
@@ -15,8 +15,6 @@ import org.esupportail.cas.adaptors.esupotp.EsupOtpService;
 import org.esupportail.cas.config.EsupOtpConfigurationProperties;
 import org.esupportail.cas.configuration.model.support.mfa.EsupOtpMultifactorProperties;
 import org.json.JSONObject;
-import org.springframework.webflow.execution.RequestContext;
-import org.springframework.webflow.execution.RequestContextHolder;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -42,12 +40,7 @@ public class EsupOtpBypassProvider extends BaseMultifactorAuthenticationProvider
 			HttpServletRequest request) {
 		try {					
 			log.debug("mfa-esupotp bypass evaluation ...");		
-			final RequestContext context = RequestContextHolder.getRequestContext();
-			if(context == null) {
-				log.trace("Not in flow context");
-				return false;
-			} else {
-				final String uid = WebUtils.getAuthentication(context).getPrincipal().getId();
+				final String uid = authentication.getPrincipal().getId();
 	
 				JSONObject userInfos = esupOtpService.getUserInfos(uid);
 				List<EsupOtpMethod> listMethods = new ArrayList<EsupOtpMethod>();
@@ -64,7 +57,6 @@ public class EsupOtpBypassProvider extends BaseMultifactorAuthenticationProvider
 					log.info(String.format("no method active for %s for service %s - mfa-esupotp bypass", uid, registeredService.getId()));
 					return false;
 				}
-			}
 		} catch (Exception e) {
 			log.error("Exception ...", e);
 			return false;
